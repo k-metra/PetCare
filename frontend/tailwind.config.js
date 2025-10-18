2cf6bc3b634ca2ef507addcc09a064a4b7c12f7a
k-metra <metrakurt@gmail.com>
2025-10-18 08:13:38 +0800

    add brand colors to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary-color": "var(--primary-color)",
+        "brand-primary": {
+          DEFAULT: "#1BA3AB",
+          100: "#A7E3E6",
+          200: "#7ED9DD",
+          300: "#55CED4",
+          400: "#2CC4CA",
+          500: "#1BA3AB",
+          600: "#16838A",
+          700: "#116269",
+          800: "#0B4149",
+          900: "#061F28",
+        }
       }
     },
   },
