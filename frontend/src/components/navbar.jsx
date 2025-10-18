1cbc28e11dca08856fa2c0a8fd21aa2c674e4ecb
k-metra <metrakurt@gmail.com>
2025-10-18 08:18:57 +0800

    enhance sidebar look

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -6,9 +6,15 @@ import { useState, useRef, useEffect } from 'react';
 export default function NavBar() {
     const [sidebarOpen, setSidebarOpen] = useState(false);
     const sidebarRef = useRef(null);
+    const menuButton = useRef(null);
 
     useEffect(() => {
         const handleClickOutside = (e) => {
+
+            if (menuButton.current && menuButton.current.contains(e.target)) {
+                return;
+            }
+
             if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
                 setSidebarOpen(false);
             }
@@ -32,11 +38,11 @@ export default function NavBar() {
         </nav>
 
         <div className="md:hidden flex">
-            <button onClick={() => setSidebarOpen(prev => !prev)}>
+            <button ref={menuButton} onClick={() => setSidebarOpen(prev => !prev)}>
                 <GiHamburgerMenu className="text-3xl text-white"/>
             </button>
         </div>
-        <div ref={sidebarRef} className={`md:hidden flex flex-col bg-brand-primary-200 px-2 gap-3 py-2 h-screen w-2/3 absolute top-[64px] right-0 shadow-lg transform transition-transform duration-300 ease-in-out ${sidebarOpen ? "translate-x-0" : "translate-x-full"}`}>
+        <div ref={sidebarRef} className={`md:hidden drop-shadow-lg rounded-sm flex flex-col bg-gray-100 px-2 gap-6 py-2 h-screen w-2/3 absolute top-[64px] right-0 shadow-lg transform transition-transform duration-300 ease-in-out ${sidebarOpen ? "translate-x-0" : "translate-x-full"}`}>
             <NavItem href="/">Home</NavItem>
             <NavItem href="/about">About</NavItem>
             <NavItem href="/services">Services</NavItem>
