2361dcc2acc10294d7556f00a3853ca9ea93855c
k-metra <metrakurt@gmail.com>
2025-10-18 10:08:47 +0800

    add services section to home page

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -3,8 +3,15 @@ import NavBar from "../components/navbar"
 import NavItem from "../components/navitem"
 
 import home_image from "../assets/home/home_image.png"
+import bg_service from "../assets/home/bg_service.jpg"
 
 import { motion } from "framer-motion"
+import Service from "../components/service"
+
+import { TbCheckupList } from "react-icons/tb";
+import { MdOutlineVaccines } from "react-icons/md";
+import { RiScissorsFill } from "react-icons/ri";
+import { TbDental } from "react-icons/tb";
 
 export default function Home() {
     return (
@@ -45,6 +52,48 @@ export default function Home() {
                     <h2 className="text-2xl md:text-3xl font-bold text-white drop-shadow-lg text-center mb-8">About Us</h2>
                     <p className="text-md md:text-lg text-white drop-shadow-md mb-6">Pet Medics Veterinary Clinic is a trusted veterinary healthcare provider based in Muntinlupa City, dedicated to delivering high-quality, compassionate, and professional care for companion animals. With a commitment to animal wellness, our clinic offers a safe and supportive environment where pets receive thorough medical attention, and pet owners are guided with empathy and expertise.</p>
                     <p className="text-md md:text-lg text-white drop-shadow-md mb-6">We are dedicated to providing the best care for your pets. Our team of experienced veterinarians and support staff are here to help you with all your pet's needs.</p>
+                </motion.div>
+            </section>
+
+            <section id="services" className="h-full w-full text-center flex flex-col justify-center items-center bg-gradient-to-b px-6 md:px-32 from-[#0E3B3E] to-[#082726]">
+                {/* Services Section */}
+                <img src={bg_service} alt="Background Service" className="absolute w-full h-full object-fill opacity-20 pointer-events-none"></img>
+
+                <motion.div
+                    className="w-full text-white"
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                >
+                    <h2 className="text-2xl md:text-3xl font-bold drop-shadow-lg text-center mb-8">Our Services</h2>
+                </motion.div>
+                <motion.div
+                    className="w-full grid grid-cols-1 md:grid-cols-3 gap-6 place-content-center place-items-center justify-center text-white"
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: true }}
+                >
+                    <Service
+                        icon={<TbCheckupList className="text-4xl mx-auto mb-4"/>}
+                        title="General Check-ups"
+                        description="Comprehensive health examinations to ensure your pet's well-being."
+                        />
+                    <Service 
+                        icon={<MdOutlineVaccines className="text-4xl mx-auto mb-4"/>}
+                        title="Vaccinations"
+                        description="Stay up-to-date with your pet's vaccinations and preventive care."
+                    />
+                    <Service 
+                        icon={<RiScissorsFill className="text-4xl mx-auto mb-4"/>}
+                        title="Grooming Services"
+                        description="Professional grooming to keep your pet looking and feeling great."
+                    />
+                    <Service 
+                        icon={<TbDental className="text-4xl mx-auto mb-4"/>}
+                        title="Dental Care"
+                        description="Maintain your pet's oral health with our dental care services."
+                        additional="col-span-full"
+                    />
                 </motion.div>
             </section>
             </main>
