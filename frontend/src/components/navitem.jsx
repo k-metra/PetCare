3d8eae88026ae158c48be29eb858bfde0241bc61
k-metra <metrakurt@gmail.com>
2025-10-18 08:13:59 +0800

    navitem text alignment to right

--- frontend/src/components/navitem.jsx
+++ frontend/src/components/navitem.jsx
@@ -1,5 +1,5 @@
 export default function NavItem({ children, href }) {
     return (
-        <li><a className="hover:underline underline-offset-8 decoration-2 decoration-white/70 transition-all duration-200 ease-out cursor-pointer px-3" href={href}>{children}</a></li>
+        <li className="list-none block"><a className="hover:underline underline-offset-8 text-right block decoration-2 decoration-white/70 transition-all duration-200 ease-out cursor-pointer px-3" href={href}>{children}</a></li>
     )
 }
