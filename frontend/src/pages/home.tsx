22aac6a260209ff7224b35b89800e34b2fab7bc2
k-metra <metrakurt@gmail.com>
2025-10-18 08:14:12 +0800

    implement definitive width and height for header

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -4,7 +4,7 @@ import NavItem from "../components/navitem"
 
 export default function Home() {
     return (
-        <header className="fixed p-4 px-7 left-0 bg-[#1BA3AB] flex flex-row justify-between items-center top-0 w-screen min-h-[5%] ">
+        <header className="fixed p-4 px-7 left-0 bg-[#1BA3AB] flex flex-row justify-between items-center top-0 w-screen h-16 md:h-20 ">
             <HeroIcon />
 
             <NavBar />
