a19e3ac10f97548aa1c14866e9c27143ecec6ff9
k-metra <metrakurt@gmail.com>
2025-10-19 20:47:18 +0800

    add client-side route path to login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Home from './pages/home';
+import Login from './pages/login';
 
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="" element={<Navigate to="/home" />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
