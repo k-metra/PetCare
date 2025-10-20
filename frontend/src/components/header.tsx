08423d379cccc6a93cd27960132a021dd108104f
k-metra <metrakurt@gmail.com>
2025-10-20 10:28:07 +0800

    feat(header): implement user state management and display email in header

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,12 +1,40 @@
 import HeroIcon from "./HeroIcon"
 import NavBar from "./navbar"
 
+import { FaUserAlt } from "react-icons/fa";
+
+import { useState, useEffect } from 'react';
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    email_verified_at: string | null;
+    created_at: string;
+    updated_at: string;
+}
+
 export default function Header() {
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const userData = localStorage.getItem('user');
+        if (userData) {
+            setUser(JSON.parse(userData));
+        }
+    }, []);
+
     return (
         <header className="fixed p-4 px-7 left-0 bg-[#1BA3AB] flex flex-row justify-between items-center top-0 w-screen h-16 md:h-20 z-50">
             <HeroIcon />
 
-            <NavBar />
+            <div className="flex flex-row items-center gap-4">
+                <NavBar />
+               
+               {user && (
+                <button className="text-white hidden md:block"><FaUserAlt className="inline mr-3"/>{user.email}</button>
+               )}
+            </div>
         </header>
     )
 }
