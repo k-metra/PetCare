0707e45343ab3fc601935f71caa1ca262a3f3686
k-metra <metrakurt@gmail.com>
2025-11-02 20:26:51 +0800

    feat(ContactFormMail): create Mailable class for handling contact form submissions

---
+++ backend/app/Mail/ContactFormMail.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ContactFormMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($contactFormData)
+    {
+        $this->contactFormData = $contactFormData;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Contact Form Mail - PetMedics Veterinary Clinic',
+        );
+    }
+
+    public function build() {
+        return $this->subject('Contact Form Mail - PetMedics Veterinary Clinic')
+                    ->view('emails.contact-form');
+    }
+
+    /**
+     * Get the message content definition.
+     */
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
